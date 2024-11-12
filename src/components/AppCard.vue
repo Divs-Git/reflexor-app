26339666a10597bf61b184201f48d8cba45651fd
Divs-Git <divyansh.sri258@gmail.com>
2024-11-12 23:15:11 +0530

    added block

---
+++ src/components/AppCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="block" v-if="showBlock">Click Me</div>
+</template>
+
+<script>
+export default {
+  name: 'AppCard',
+  props: {
+    delay: {
+      type: Number,
+    },
+  },
+  data() {
+    return {
+      showBlock: false,
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true
+    }, this.delay)
+  },
+}
+</script>
+
+<style scoped>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background-color: var(--bg-card);
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
